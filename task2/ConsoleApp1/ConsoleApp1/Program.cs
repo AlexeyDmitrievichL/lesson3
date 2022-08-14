1407eaa5638aa5cd46ee568acb46390639aa3390
Alexey Lapushinsky <alexey.lapushinsky@solbeg.com>
2022-08-14 20:08:32 +0300

    added task2 to lesson3

---
+++ task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            getNumbers();
+        }
+
+        private static void getNumbers()
+        {
+            Console.WriteLine("Input first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            compareNumbers(firstNumber, secondNumber);
+            getNumbers();
+        }
+
+        private static void compareNumbers(in int numberA, in int numberB)
+        {
+            if (numberA > numberB)
+            {
+                Console.WriteLine($"Number {numberA} more then number {numberB}");
+            }
+
+            if (numberB > numberA)
+            {
+                Console.WriteLine($"Number {numberB} more then number {numberA}");
+            }
+
+            if (numberB == numberA)
+            {
+                Console.WriteLine($"Number {numberA} is equal to number {numberB}");
+            }
+        }
+    }
+}
