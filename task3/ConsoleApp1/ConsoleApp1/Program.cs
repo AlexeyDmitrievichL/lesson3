649872a77890173bc6b6623fc308fd0918dae819
Alexey Lapushinsky <alexey.lapushinsky@solbeg.com>
2022-08-14 21:30:12 +0300

    added task3 to lesson3

---
+++ task3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number, bro :)");
+            string userInput = Console.ReadLine();
+            checkOnPalindrom(userInput);
+            Console.ReadKey();
+        }
+
+        public static string reverseString(string userInput)
+        {
+            char[] charArray = userInput.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private static void checkOnPalindrom(in string userInput)
+        {
+            int userInputNumber = Convert.ToInt32(userInput);
+            if (userInputNumber >= 0 && userInputNumber < 10)
+            {
+                Console.WriteLine($"{userInput} is palindrom");
+                return;
+            }
+            int reversedUserInputNumber = Convert.ToInt32(reverseString(userInput));
+            if (userInputNumber / reversedUserInputNumber == 1)
+                Console.WriteLine($"{userInput} is palindrom");
+            else
+                Console.WriteLine($"{userInput} is not palindrom");
+
+        }
+    }
+}
