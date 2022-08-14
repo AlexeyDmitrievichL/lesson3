2054aa8205a7607a40c249747344c7aa01f44d3f
Alexey Lapushinsky <alexey.lapushinsky@solbeg.com>
2022-08-14 19:20:20 +0300

    added task1 to lesson3

---
+++ task1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Define A");
+            int firstVar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Define B");
+            int secondVar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A = {firstVar}; B = {secondVar}");
+            Console.WriteLine("Some magic and...");
+            doSomeMagic(ref firstVar, ref secondVar);
+            Console.WriteLine($"A = {firstVar}; B = {secondVar}");
+            Console.WriteLine("Press any key for exit, bro :)");
+            Console.ReadKey();
+        }
+
+        private static void doSomeMagic(ref int a, ref int b)
+        {
+            a += b;
+            b = a - b;
+            a -= b;
+        }
+    }
+}
